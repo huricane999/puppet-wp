e45d0b95a297a9fda56403476c922715029ab035
Chris Smith <huricane.smithy@gmail.com>
2016-11-17 00:59:01 -0500

    Add support for extra-php parameter and multisite
    Added support for wp-cli's --extra-php parameter to add raw php to
    wp-config.php.
    
    Also added a template that generates php code for multisite
    configuration that is added to the extra-php code

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,16 +1,35 @@
 define wp::config (
   $location = $title,
-  $dbname    = 'wordpress',
-  $dbuser    = 'wordpress',
-  $dbpass    = 'P@ssw0rd',
-  $dbhost    = 'localhost',
-  $dbprefix  = 'wp_',
-  $dbcharset = 'utf8',
-  $user      = $::wp::user
+  $dbname       = 'wordpress',
+  $dbuser       = 'wordpress',
+  $dbpass       = 'P@ssw0rd',
+  $dbhost       = 'localhost',
+  $dbprefix     = 'wp_',
+  $dbcharset    = 'utf8',
+  $multisite    = false,
+  $ms_subdomain = false,
+  $ms_domain    = '',
+  $ms_path      = '/',
+  $ms_site_id   = 1,
+  $ms_blog_id   = 1,
+  $extraphp     = '',
+  $user         = $::wp::user
 ) {
   include wp::cli
+  
+  if $multisite {
+    $multisitephp = template( 'wp/config_extraphp_multisite.erb' )
+  } else {
+    $multisitephp = ''
+  }
 
-  $config = "config --path='${location}' --dbname='${dbname}' --dbuser='${dbuser}' --dbpass='${dbpass}' --dbhost='${dbhost}' --dbprefix='${dbprefix}'"
+  $extraphp_str = "${multisitephp}${extraphp}"
+
+  if $extraphp_str != '' {
+    $config = "config --path='${location}' --dbname='${dbname}' --dbuser='${dbuser}' --dbpass='${dbpass}' --dbhost='${dbhost}' --dbprefix='${dbprefix}' --extra-php <<PHP\n${extraphp_str}\nPHP"
+  } else {
+    $config = "config --path='${location}' --dbname='${dbname}' --dbuser='${dbuser}' --dbpass='${dbpass}' --dbhost='${dbhost}' --dbprefix='${dbprefix}'"
+  }
   
   exec {"wp config ${location}":
     command => "/usr/bin/wp core ${config}",
