8b8344705cf67e3d6eb7bb6af4e186be7eebbf6e
Daniel Henninger <daniel@ncsu.edu>
2017-04-15 18:23:07 -0400

    Importing upstream (well double upstream) work on the plugin component.

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -3,44 +3,69 @@ define wp::plugin (
   $location,
   $ensure = enabled,
   $networkwide = false,
-  $user = $::wp::user
+  $version = 'latest',
+  $user = $::wp::user,
 ) {
   include wp::cli
 
+  if ( $networkwide ) {
+    $network = ' --network'
+  }
+
+  if ( $version != 'latest' ) {
+    $held = " --version=$version"
+  }
+
   case $ensure {
     enabled: {
-      $command = "activate ${slug}"
-
-      exec { "wp install plugin ${title}":
+      exec { "wp install plugin $title --activate$network$held":
         cwd     => $location,
         user    => $user,
-        command => "/usr/bin/wp plugin install ${slug}",
-        unless  => "/usr/bin/wp plugin is-installed ${slug}",
-        before  => Wp::Command["${location} plugin ${slug} ${ensure}"],
-        require => Class['wp::cli'],
-        onlyif  => '/usr/bin/wp core is-installed'
+        command => "/usr/bin/wp plugin install $slug --activate $held",
+        unless  => "/usr/bin/wp plugin is-installed $slug",
+        require => Class["wp::cli"],
+        onlyif  => "/usr/bin/wp core is-installed"
       }
     }
     disabled: {
-      $command = "deactivate ${slug}"
+      exec { "wp deactivate plugin $title$network$held":
+        cwd     => $location,
+        user    => $user,
+        command => "/usr/bin/wp plugin deactivate $slug",
+        require => Class["wp::cli"],
+        onlyif  => "/usr/bin/wp core is-installed"
+      }
     }
     installed: {
-      $command = "install ${slug}"
+      exec { "wp install plugin $title$network$held":
+        cwd     => $location,
+        user    => $user,
+        command => "/usr/bin/wp plugin install $slug --activate $held",
+        unless  => "/usr/bin/wp plugin is-installed $slug",
+        require => Class["wp::cli"],
+        onlyif  => "/usr/bin/wp core is-installed"
+      }
+    }
+    deleted: {
+      exec { "wp delete plugin $title":
+        cwd     => $location,
+        user    => $user,
+        command => "/usr/bin/wp plugin delete $slug",
+        require => Class["wp::cli"],
+        onlyif  => "/usr/bin/wp core is-installed"
+      }
+    }
+    uninstalled: {
+      exec { "wp uninstall plugin $title":
+        cwd     => $location,
+        user    => $user,
+        command => "/usr/bin/wp plugin uninstall $slug --deactivate",
+        require => Class["wp::cli"],
+        onlyif  => "/usr/bin/wp core is-installed"
+      }
     }
     default: {
-      fail('Invalid ensure for wp::plugin')
+      fail( "Invalid ensure argument passed into wp::plugin" )
     }
-  }
-
-  if $networkwide {
-    $args = "plugin ${command} --network"
-  }
-  else {
-    $args = "plugin ${command}"
-  }
-  wp::command { "${location} plugin ${slug} ${ensure}":
-    location => $location,
-    command  => $args,
-    user     => $user
   }
 }
