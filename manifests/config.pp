dd3deb2f25127c14c0d972533e797093c70202bc
Chris Smith <casmit14@ncsu.edu>
2018-05-10 11:38:03 -0400

    add config set commands

--- manifests/config.pp
+++ manifests/config.pp
@@ -43,12 +43,94 @@ define wp::config (
     path    => '/bin:/usr/bin:/usr/sbin',
     creates => "${location}/wp-config.php",
   }
-  
-  exec { "${location} wp config set dbname":
-    command => "/usr/bin/wp config set --path='${location}' dbname '${dbname}'",
-    user    => $user,
-    require => [ Class['wp::cli'] ],
-    path    => '/bin:/usr/bin:/usr/sbin',
-    unless  => "/bin/test \"`/usr/bin/wp config get --path='${location}' dbname`\" == '${dbname}'",
+
+  wp::command { "${location} wp config set dbname":
+    location => $location,
+    command  => "config set DB_NAME '${dbname}'",
+    user     => $user,
+    unless   => "/bin/test \"`/usr/bin/wp config get DB_NAME`\" == '${dbname}'",
+  }
+
+  wp::command { "${location} wp config set dbuser":
+    location => $location,
+    command  => "config set DB_USER '${dbuser}'",
+    user     => $user,
+    unless   => "/bin/test \"`/usr/bin/wp config get DB_USER`\" == '${dbuser}'",
+  }
+
+  wp::command { "${location} wp config set dbpass":
+    location => $location,
+    command  => "config set DB_PASSWORD '${dbpass}'",
+    user     => $user,
+    unless   => "/bin/test \"`/usr/bin/wp config get DB_PASSWORD`\" == '${dbpass}'",
+  }
+
+  wp::command { "${location} wp config set dbprefix":
+    location => $location,
+    command  => "config set table_prefix '${dbprefix}'",
+    user     => $user,
+    unless   => "/bin/test \"`/usr/bin/wp config get table_prefix`\" == '${dbprefix}'",
+  }
+
+  $multisite_str = bool2str($multisite)
+  if $multisite {
+    $multisite_int = '1'
+  } else {
+    $multisite_int = '0'
+  }
+
+  wp::command { "${location} wp config set WP_ALLOW_MULTISITE":
+    location => $location,
+    command  => "config set WP_ALLOW_MULTISITE --raw ${multisite_str}",
+    user     => $user,
+    unless   => "/bin/test \"`/usr/bin/wp config get WP_ALLOW_MULTISITE`\" == '${multisite_int}'",
+  }
+
+  wp::command { "${location} wp config set MULTISITE":
+    location => $location,
+    command  => "config set MULTISITE --raw ${multisite_str}",
+    user     => $user,
+    unless   => "/bin/test \"`/usr/bin/wp config get MULTISITE`\" == '${multisite_int}'",
+  }
+
+  $ms_subdomain_str = bool2str($ms_subdomain)
+  if $ms_subdomain {
+    $ms_subdomain_int = '1'
+  } else {
+    $ms_subdomain_int = '0'
+  }
+  wp::command { "${location} wp config set SUBDOMAIN_INSTALL":
+    location => $location,
+    command  => "config set SUBDOMAIN_INSTALL --raw ${ms_subdomain_str}",
+    user     => $user,
+    unless   => "/bin/test \"`/usr/bin/wp config get SUBDOMAIN_INSTALL`\" == '${ms_subdomain_int}'",
+  }
+
+  wp::command { "${location} wp config set DOMAIN_CURRENT_SITE":
+    location => $location,
+    command  => "config set DOMAIN_CURRENT_SITE '${ms_domain}'",
+    user     => $user,
+    unless   => "/bin/test \"`/usr/bin/wp config get DOMAIN_CURRENT_SITE`\" == '${ms_domain}'",
+  }
+
+  wp::command { "${location} wp config set PATH_CURRENT_SITE":
+    location => $location,
+    command  => "config set PATH_CURRENT_SITE '${ms_path}'",
+    user     => $user,
+    unless   => "/bin/test \"`/usr/bin/wp config get PATH_CURRENT_SITE`\" == '${ms_path}'",
+  }
+
+  wp::command { "${location} wp config set SITE_ID_CURRENT_SITE":
+    location => $location,
+    command  => "config set SITE_ID_CURRENT_SITE '${ms_site_id}'",
+    user     => $user,
+    unless   => "/bin/test \"`/usr/bin/wp config get --path='${location}' SITE_ID_CURRENT_SITE`\" == '${ms_site_id}'",
+  }
+
+  wp::command { "${location} wp config set BLOG_ID_CURRENT_SITE":
+    location => $location,
+    command  => "config set BLOG_ID_CURRENT_SITE '${ms_blog_id}'",
+    user     => $user,
+    unless   => "/bin/test \"`/usr/bin/wp config get --path='${location}' BLOG_ID_CURRENT_SITE`\" == '${ms_blog_id}'",
   }
 }
