287606a600e3d7b28e6171d498ad6ba7eb1de7b5
Chris Smith <casmit14@ncsu.edu>
2018-05-11 16:36:55 -0400

    use exec for deactivate theme

--- manifests/theme.pp
+++ manifests/theme.pp
@@ -51,12 +51,12 @@ define wp::theme (
         onlyif   => "/usr/bin/wp theme status ${theme_name} --skip-plugins --skip-themes --skip-packages | grep -q Status:\\ Inactive",
         tag      => 'theme-uninstalled',
       }
-      ->wp::command { "${location} deactivate theme ${theme_name}":
-        location => $location,
-        command  => "theme activate \"$(/usr/bin/wp theme list --skip-plugins --skip-themes --skip-packages | grep -e \\\"network\|site\\\" | awk \\\"{print \\\$1}\\\")\" --skip-plugins --skip-themes --skip-packages",
-        user     => $user,
-        onlyif   => "/usr/bin/wp theme list --skip-plugins --skip-themes --skip-packages | grep -e ${theme_name} | grep -q active",
-        tag      => 'theme-uninstalled',
+      ->exec { "${location} deactivate theme ${theme_name}":
+        command => "/bin/bash -c '/usr/bin/wp theme activate \"$(/usr/bin/wp theme list --skip-plugins --skip-themes --skip-packages | grep -e \\\"network\|site\\\" | awk \\\"{print \\\$1}\\\")\" --skip-plugins --skip-themes --skip-packages'",
+        cwd     => $location,
+        user    => $user,
+        onlyif  => "/usr/bin/wp theme list --skip-plugins --skip-themes --skip-packages | grep -e ${theme_name} | grep -q active",
+        tag     => 'theme-uninstalled',
       }
       if $networkwide {
         # lint:ignore:140chars
@@ -65,7 +65,7 @@ define wp::theme (
           cwd     => $location,
           user    => $user,
           unless  => "/bin/bash -c 'ret=0; while read line; do /usr/bin/wp --allow-root theme list --url=\$line --skip-plugins --skip-themes --skip-packages | grep -e ${theme_name} | grep -q \"network\|site\"; if [ $? -eq 0 ]; then let \"ret++\"; fi; done <<< \"$(/usr/bin/wp --allow-root site list --field=url --skip-plugins --skip-themes --skip-packages)\"; echo \$ret; /bin/test \$ret == 0'",
-          require => Wp::Command["${location} deactivate theme ${theme_name}"],
+          require => Exec["${location} deactivate theme ${theme_name}"],
           before  => Wp::Command["${location} theme ${command}"],
           tag     => 'theme-uninstalled',
         }
