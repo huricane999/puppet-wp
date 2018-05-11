547422e2fd210e637b7fe5ace6fab8fbb577b53f
Chris Smith <casmit14@ncsu.edu>
2018-05-11 14:14:22 -0400

    nested script syntax error

--- manifests/theme.pp
+++ manifests/theme.pp
@@ -46,7 +46,7 @@ define wp::theme (
       if $networkwide {
         # lint:ignore:140chars
         exec { "${location} network deactivate theme ${theme_name}":
-          command => "/bin/bash -c 'while read line; do /usr/bin/wp theme activate \"$(wp theme list --url=\$line --skip-plugins --skip-themes --skip-packages | grep -e site | awk \\\'{print \$1}\\\')\" --url=\$line --skip-plugins --skip-themes --skip-packages; done <<< \"$(/usr/bin/wp site list --field=url --skip-plugins --skip-themes --skip-packages)\"'",
+          command => "/bin/bash -c 'while read line; do /usr/bin/wp theme activate $(/usr/bin/wp theme list --url=\$line --skip-plugins --skip-themes --skip-packages | grep -e \"network\|site\" | awk \"{print \\\$1}\") --url=\$line --skip-plugins --skip-themes --skip-packages; done <<< \"$( /usr/bin/wp site list --field=url --skip-plugins --skip-themes --skip-packages )\"'",
           cwd     => $location,
           user    => $user,
           unless  => "/bin/bash -c 'ret=0; while read line; do /usr/bin/wp theme status ${theme_name} --url=\$line --skip-plugins --skip-themes --skip-packages | grep Status | grep -q Active; if [ $? -eq 0 ]; then let \"ret++\"; fi; done <<< \"$(/usr/bin/wp site list --field=url --skip-plugins --skip-themes --skip-packages)\"; /bin/test \$ret == 0'",
@@ -58,7 +58,7 @@ define wp::theme (
           command => "/bin/bash -c 'while read line; do /usr/bin/wp theme disable ${theme_name} --url=\$line --skip-plugins --skip-themes --skip-packages; done <<< \"$(/usr/bin/wp site list --field=url --skip-plugins --skip-themes --skip-packages)\"'",
           cwd     => $location,
           user    => $user,
-          unless  => "/bin/bash -c 'ret=0; while read line; do /usr/bin/wp --allow-root theme list --url=\$line --skip-plugins --skip-themes --skip-packages | grep -e ${theme_name} | grep -q site; if [ $? -eq 0 ]; then let \"ret++\"; fi; done <<< \"$(/usr/bin/wp --allow-root site list --field=url --skip-plugins --skip-themes --skip-packages)\"; echo \$ret; /bin/test \$ret == 0'",
+          unless  => "/bin/bash -c 'ret=0; while read line; do /usr/bin/wp --allow-root theme list --url=\$line --skip-plugins --skip-themes --skip-packages | grep -e ${theme_name} | grep -q \"network\|site\"; if [ $? -eq 0 ]; then let \"ret++\"; fi; done <<< \"$(/usr/bin/wp --allow-root site list --field=url --skip-plugins --skip-themes --skip-packages)\"; echo \$ret; /bin/test \$ret == 0'",
           require => Class['wp::cli'],
           before  => Wp::Command["${location} theme ${command}"],
           tag     => 'uninstalled',
