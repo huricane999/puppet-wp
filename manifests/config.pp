efa341162cd45445524ecb00c9384daba48f5a28
Chris Smith <casmit14@ncsu.edu>
2018-05-15 11:13:24 -0400

    delete multisite config options when not used

--- manifests/config.pp
+++ manifests/config.pp
@@ -83,61 +83,108 @@ define wp::config (
 
   if $multisite {
     $multisite_int = '1'
+
+    wp::command { "${location} wp config set WP_ALLOW_MULTISITE":
+      location => $location,
+      command  => "config set WP_ALLOW_MULTISITE --raw ${multisite_int} --type=constant",
+      user     => $user,
+      unless   => "/bin/test \"`/usr/bin/wp config get WP_ALLOW_MULTISITE`\" == '${multisite_int}'",
+    }
+
+    wp::command { "${location} wp config set MULTISITE":
+      location => $location,
+      command  => "config set MULTISITE --raw ${multisite_int} --type=constant",
+      user     => $user,
+      unless   => "/bin/test \"`/usr/bin/wp config get MULTISITE`\" == '${multisite_int}'",
+    }
+
+    if $ms_subdomain {
+      $ms_subdomain_int = '1'
+    } else {
+      $ms_subdomain_int = '0'
+    }
+    wp::command { "${location} wp config set SUBDOMAIN_INSTALL":
+      location => $location,
+      command  => "config set SUBDOMAIN_INSTALL --raw ${ms_subdomain_int} --type=constant",
+      user     => $user,
+      unless   => "/bin/test \"`/usr/bin/wp config get SUBDOMAIN_INSTALL`\" == '${ms_subdomain_int}'",
+    }
+
+    wp::command { "${location} wp config set DOMAIN_CURRENT_SITE":
+      location => $location,
+      command  => "config set DOMAIN_CURRENT_SITE '${ms_domain}' --type=constant",
+      user     => $user,
+      unless   => "/bin/test \"`/usr/bin/wp config get DOMAIN_CURRENT_SITE`\" == '${ms_domain}'",
+    }
+
+    wp::command { "${location} wp config set PATH_CURRENT_SITE":
+      location => $location,
+      command  => "config set PATH_CURRENT_SITE '${ms_path}' --type=constant",
+      user     => $user,
+      unless   => "/bin/test \"`/usr/bin/wp config get PATH_CURRENT_SITE`\" == '${ms_path}'",
+    }
+
+    wp::command { "${location} wp config set SITE_ID_CURRENT_SITE":
+      location => $location,
+      command  => "config set SITE_ID_CURRENT_SITE '${ms_site_id}' --type=constant",
+      user     => $user,
+      unless   => "/bin/test \"`/usr/bin/wp config get --path='${location}' SITE_ID_CURRENT_SITE`\" == '${ms_site_id}'",
+    }
+
+    wp::command { "${location} wp config set BLOG_ID_CURRENT_SITE":
+      location => $location,
+      command  => "config set BLOG_ID_CURRENT_SITE '${ms_blog_id}' --type=constant",
+      user     => $user,
+      unless   => "/bin/test \"`/usr/bin/wp config get --path='${location}' BLOG_ID_CURRENT_SITE`\" == '${ms_blog_id}'",
+    }
   } else {
-    $multisite_int = '0'
-  }
+    wp::command { "${location} wp config delete WP_ALLOW_MULTISITE":
+      location => $location,
+      command  => 'config delete WP_ALLOW_MULTISITE',
+      user     => $user,
+      unless   => '/usr/bin/wp config has WP_ALLOW_MULTISITE',
+    }
 
-  wp::command { "${location} wp config set WP_ALLOW_MULTISITE":
-    location => $location,
-    command  => "config set WP_ALLOW_MULTISITE --raw ${multisite_int} --type=constant",
-    user     => $user,
-    unless   => "/bin/test \"`/usr/bin/wp config get WP_ALLOW_MULTISITE`\" == '${multisite_int}'",
-  }
+    wp::command { "${location} wp config delete MULTISITE":
+      location => $location,
+      command  => 'config delete MULTISITE',
+      user     => $user,
+      unless   => '/usr/bin/wp config has MULTISITE',
+    }
 
-  wp::command { "${location} wp config set MULTISITE":
-    location => $location,
-    command  => "config set MULTISITE --raw ${multisite_int} --type=constant",
-    user     => $user,
-    unless   => "/bin/test \"`/usr/bin/wp config get MULTISITE`\" == '${multisite_int}'",
-  }
+    wp::command { "${location} wp config delete SUBDOMAIN_INSTALL":
+      location => $location,
+      command  => 'config delete SUBDOMAIN_INSTALL',
+      user     => $user,
+      unless   => '/usr/bin/wp config has SUBDOMAIN_INSTALL',
+    }
 
-  if $ms_subdomain {
-    $ms_subdomain_int = '1'
-  } else {
-    $ms_subdomain_int = '0'
-  }
-  wp::command { "${location} wp config set SUBDOMAIN_INSTALL":
-    location => $location,
-    command  => "config set SUBDOMAIN_INSTALL --raw ${ms_subdomain_int} --type=constant",
-    user     => $user,
-    unless   => "/bin/test \"`/usr/bin/wp config get SUBDOMAIN_INSTALL`\" == '${ms_subdomain_int}'",
-  }
+    wp::command { "${location} wp config delete DOMAIN_CURRENT_SITE":
+      location => $location,
+      command  => 'config delete DOMAIN_CURRENT_SITE',
+      user     => $user,
+      unless   => '/usr/bin/wp config has DOMAIN_CURRENT_SITE',
+    }
 
-  wp::command { "${location} wp config set DOMAIN_CURRENT_SITE":
-    location => $location,
-    command  => "config set DOMAIN_CURRENT_SITE '${ms_domain}' --type=constant",
-    user     => $user,
-    unless   => "/bin/test \"`/usr/bin/wp config get DOMAIN_CURRENT_SITE`\" == '${ms_domain}'",
-  }
+    wp::command { "${location} wp config delete PATH_CURRENT_SITE":
+      location => $location,
+      command  => 'config delete PATH_CURRENT_SITE',
+      user     => $user,
+      unless   => '/usr/bin/wp config has PATH_CURRENT_SITE',
+    }
 
-  wp::command { "${location} wp config set PATH_CURRENT_SITE":
-    location => $location,
-    command  => "config set PATH_CURRENT_SITE '${ms_path}' --type=constant",
-    user     => $user,
-    unless   => "/bin/test \"`/usr/bin/wp config get PATH_CURRENT_SITE`\" == '${ms_path}'",
-  }
+    wp::command { "${location} wp config delete SITE_ID_CURRENT_SITE":
+      location => $location,
+      command  => 'config delete SITE_ID_CURRENT_SITE',
+      user     => $user,
+      unless   => '/usr/bin/wp config has SITE_ID_CURRENT_SITE',
+    }
 
-  wp::command { "${location} wp config set SITE_ID_CURRENT_SITE":
-    location => $location,
-    command  => "config set SITE_ID_CURRENT_SITE '${ms_site_id}' --type=constant",
-    user     => $user,
-    unless   => "/bin/test \"`/usr/bin/wp config get --path='${location}' SITE_ID_CURRENT_SITE`\" == '${ms_site_id}'",
-  }
-
-  wp::command { "${location} wp config set BLOG_ID_CURRENT_SITE":
-    location => $location,
-    command  => "config set BLOG_ID_CURRENT_SITE '${ms_blog_id}' --type=constant",
-    user     => $user,
-    unless   => "/bin/test \"`/usr/bin/wp config get --path='${location}' BLOG_ID_CURRENT_SITE`\" == '${ms_blog_id}'",
+    wp::command { "${location} wp config delete BLOG_ID_CURRENT_SITE":
+      location => $location,
+      command  => 'config delete BLOG_ID_CURRENT_SITE',
+      user     => $user,
+      unless   => '/usr/bin/wp config has BLOG_ID_CURRENT_SITE',
+    }
   }
 }
