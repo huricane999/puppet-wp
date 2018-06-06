80bf53764dced459b96bd26133c7a85109d98100
Chris Smith <casmit14@ncsu.edu>
2018-06-06 15:30:07 -0400

    ordering

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -155,9 +155,9 @@ define wp::plugin (
     }
   }
 
-  Wp::Command<| tag == 'plugin-uninstalled' |>
+  Wp::Command<| tag == 'plugin-disabled' |>
+  -> Wp::Command<| tag == 'plugin-uninstalled' |>
   -> Wp::Command<| tag == 'plugin-installed' |>
-  -> Wp::Command<| tag == 'plugin-disabled' |>
   -> Wp::Command<| tag == 'plugin-enabled' |>
   # lint:endignore
 }
